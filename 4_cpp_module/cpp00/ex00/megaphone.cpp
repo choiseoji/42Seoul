d39e226f58cdf65dbaa074853f9f4486515f8ebf
Seoji Choi <seojchoi@c3r6s4.42seoul.kr>
2023-12-05 16:11:08 +0900

    [feat]: every char to upper char -> megaphone.cpp

---
+++ 4_cpp_module/cpp00/ex00/megaphone.cpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seojchoi <seojchoi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/05 14:47:03 by seojchoi          #+#    #+#             */
+/*   Updated: 2023/12/05 16:01:19 by seojchoi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+void	check_is_empty(int ac)
+{
+	if (ac == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	}
+}
+
+int main(int ac, char **av)
+{
+	int	cnt;
+	int	idx;
+
+	check_is_empty(ac);
+	cnt = 1;
+	while (cnt < ac)
+	{
+		idx = 0;
+		while (av[cnt][idx])
+		{
+			std::cout << char(toupper(av[cnt][idx]));
+			idx++;
+		}
+		cnt++;
+	}
+	std::cout << "\n";
+	return (0);
+}
