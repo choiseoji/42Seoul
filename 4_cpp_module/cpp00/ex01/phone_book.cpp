cd8d280d40e7336251f46b5cb91deade560ca52b
최서지 <seoji@MacBook-Air.local>
2023-12-06 23:17:53 +0900

    [feat]: 형식에 맞게 출력 문구 추가(오른쪽으로 정렬 사용)

--- 4_cpp_module/cpp00/ex01/phone_book.cpp
+++ 4_cpp_module/cpp00/ex01/phone_book.cpp
@@ -40,16 +40,17 @@ Contact::Contact(std::string first_name, std::string last_name, std::string nick
 // 9번째 contact는 첫번째 contact 자리를 대체한다
 class PhoneBook
 {
-	private:
-		int		cur_idx;
 	public:
 		PhoneBook();   // 생성자
+		int		cur_idx;
 		Contact arr[8];
 
 		void	Add(Contact &);
 };
 
-PhoneBook::PhoneBook() {}
+PhoneBook::PhoneBook() {
+	this->cur_idx = 0;
+}
 
 void	PhoneBook::Add(Contact &new_contact)
 {
@@ -72,7 +73,7 @@ int main(void)
 		getline(std::cin, cmd);
 		if (cmd == "ADD")
 		{
-			std::cout << "\n==========add===========\n\n";
+			std::cout << "\n====================add====================\n\n";
 			std::cout << "first name > ";
 			getline(std::cin, first_name);
 			std::cout << "last name > ";
@@ -83,19 +84,70 @@ int main(void)
 			getline(std::cin, phone_number);
 			std::cout << "darkest secret > ";
 			getline(std::cin, darkest_secret);
-			std::cout << "\n===========save===========\n\n";
+			std::cout << "\n===================save====================\n\n";
 
 			Contact new_contact = Contact(first_name, last_name, nick_name, phone_number, darkest_secret);   // 새로운 객체 생성
 			phone_book.Add(new_contact);
 		}
 		else if (cmd == "SEARCH")
 		{
-			std::cout << "\n=========search=========\n\n";
+			std::cout << "\n==================search===================\n\n";
+
+			std::cout.width(10);
+			std::cout << std::right << "index";
+			std::cout << "|";
+			std::cout.width(10);
+			std::cout << std::right << "first name";
+			std::cout << "|";
+			std::cout.width(10);
+			std::cout << std::right << "last name";
+			std::cout << "|";
+			std::cout.width(10);
+			std::cout << std::right << "nick name";
+			std::cout << "\n";
+			std::cout << "===========================================\n";
+
 			for(int i = 0; i < 8; i++)
 			{
-				std::cout << phone_book.arr[i].first_name << "\n";
+				std::cout.width(10);
+				std::cout << std::right << i + 1;
+				std::cout << "|";
+				if (phone_book.arr[i].first_name.size() > 10)
+				{
+					std::cout << phone_book.arr[i].first_name.substr(0, 9);
+					std::cout << ".";
+				}
+				else
+				{
+					std::cout.width(10);
+					std::cout << std::right << phone_book.arr[i].first_name;
+				}
+				std::cout << "|";
+				if (phone_book.arr[i].last_name.size() > 10)
+				{
+					std::cout << std::right << phone_book.arr[i].last_name.substr(0, 9);
+					std::cout << ".";
+				}
+				else
+				{
+					std::cout.width(10);
+					std::cout << std::right << phone_book.arr[i].last_name;
+				}
+				std::cout << "|";
+				if (phone_book.arr[i].nick_name.size() > 10)
+				{
+					std::cout << std::right << phone_book.arr[i].nick_name.substr(0, 9);
+					std::cout << ".";
+				}
+				else
+				{
+					std::cout.width(10);
+					std::cout << std::right << phone_book.arr[i].nick_name;
+				}
+				std::cout << "\n";
 			}
-			std::cout << "\n==========================\n\n";
+
+			std::cout << "\n===========================================\n\n";
 		}
 		else if (cmd == "EXIT")
 			break ;
