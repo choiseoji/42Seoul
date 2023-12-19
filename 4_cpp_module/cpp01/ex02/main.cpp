68c8df7bc095ee2cc4c21d7b01c9ba6a5c30fe1d
Seoji Choi <seojchoi@c1r5s7.42seoul.kr>
2023-12-19 15:27:34 +0900

    [feat]: ex02 -> stringREF 레퍼런스 변수 추가

--- 4_cpp_module/cpp01/ex02/main.cpp
+++ 4_cpp_module/cpp01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: seojchoi <seojchoi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/18 17:25:37 by seojchoi          #+#    #+#             */
-/*   Updated: 2023/12/18 17:35:41 by seojchoi         ###   ########.fr       */
+/*   Updated: 2023/12/19 15:26:01 by seojchoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,20 @@
  int main(void)
  {
 	std::string str;
-	char		*stringPTR;
-	// char		*stringREF;
-
 	str = "HI THIS IS BRAIN";
-	stringPTR = &str[0];   // str 첫 글자의 주소를 stringPRT에 저장
 
-	std::cout << &str << std::endl;
-	std::cout << &stringPTR << std::endl;
-	// std::cout << &stringREF << std::endl;
-	std::cout << str << std::endl;
-	std::cout << stringPTR << std::endl;
-	// std::cout << stringREF << std::endl;
+	std::string *stringPTR = &str;
+	std::string &stringREF = str;
 
+	// print address
+	std::cout << "str의 address : " << &str << std::endl;
+	std::cout << "stringPRT의 address : " << &stringPTR << std::endl;
+	std::cout << "stringREF의 address : " << &stringREF << std::endl;
+
+	std::cout << std::endl;
+	// print value
+	std::cout << "str의 value : " << str << std::endl;
+	std::cout << "stringPTR의 value : " << stringPTR << std::endl;
+	std::cout << "stringREF의 value : " << stringREF << std::endl;
 	return (0);
  }
