8743911f9ee4884197b37bf6132b2e964a60da28
Seoji Choi <seojchoi@c1r5s3.42seoul.kr>
2023-12-20 15:02:22 +0900

    [feat]: replace 기능 구현은 했지만 c++스럽게 바꿔야 함

---
+++ 4_cpp_module/cpp01/ex04/Replace.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Replace.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seojchoi <seojchoi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/20 14:02:56 by seojchoi          #+#    #+#             */
+/*   Updated: 2023/12/20 14:11:24 by seojchoi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef REPLACE_HPP
+# define REPLACE_HPP
+# include <iostream>
+
+class Replace
+{
+public:
+	Replace();
+	~Replace();
+};
+
+#endif
--- 4_cpp_module/cpp01/ex04/main.cpp
+++ 4_cpp_module/cpp01/ex04/main.cpp
@@ -6,41 +6,55 @@
 /*   By: seojchoi <seojchoi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/19 19:26:49 by seojchoi          #+#    #+#             */
-/*   Updated: 2023/12/19 20:10:36 by seojchoi         ###   ########.fr       */
+/*   Updated: 2023/12/20 15:01:25 by seojchoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
+#include "Replace.hpp"
 #include <fstream>
+#include <string>
 
-void input_error(void)
+void	input_error(void)
 {
+	std::cout << "input error\n";
+}
 
+std::string	replace_s1_to_s2(std::string str, std::string s1, std::string s2)
+{
+	size_t	idx;
+
+	while (1)
+	{
+		idx = str.find(s1);  //  s1의 인덱스 반환
+		if (idx == std::string::npos)
+			break ;
+		str.erase(idx, s1.size());
+		str.insert(idx, s2);
+	}
+	return (str);
 }
 
 int main(int ac, char **av)
 {
-	std::string filename;
-	std::string s1, s2;
-	std::ifstream	ifs;
+	std::string infile = av[1];
+	std::string s1 = av[2];
+	std::string s2 = av[3];
+	std::string outfile = infile + ".replace";
 
 	if (ac != 4)
 		input_error();
-	else
+
+	std::ifstream f(infile);
+	std::ofstream out(outfile);
+	while (f)
 	{
-		// filename = av[1];
-		// s1 = av[2];
-		// s2 = av[3];
+		std::string in_str;
+		std::string out_str;
 
-		// ifs.open("test.txt");
-		// while (ifs)
-		// {
-		// 	std::string s;
-
-		// 	getline(ifs, s);
-		// 	std::cout << s;
-		// }
-		// ifs.close();
+		getline(f, in_str);
+		in_str += '\n';
+		out_str = replace_s1_to_s2(in_str, s1, s2);
+		out << out_str;
 	}
 	return (0);
 }
