839542ec186cdbd5c5e71671b0430b393e32a6f6
Seoji Choi <seojchoi@cx1r4s3.42seoul.kr>
2024-01-14 19:01:38 +0900

    [refact]: 주석한거 다시 써줌

--- 4_cpp_module/cpp02/ex03/Fixed.cpp
+++ 4_cpp_module/cpp02/ex03/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: seojchoi <seojchoi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/04 14:49:19 by seojchoi          #+#    #+#             */
-/*   Updated: 2024/01/05 21:17:08 by seojchoi         ###   ########.fr       */
+/*   Updated: 2024/01/14 19:01:01 by seojchoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,13 @@
 // 생성자
 Fixed::Fixed()
 {
-	// std::cout << "Default constructor called" << std::endl;
+	std::cout << "Default constructor called" << std::endl;
 	this->fixedPointNum = 0;
 }
 
 Fixed::Fixed(const int num)
 {
-	// std::cout << "Int constructor called" << std::endl;
+	std::cout << "Int constructor called" << std::endl;
 	this->fixedPointNum = num * (1 << this->FractionalBits);
 }
 
@@ -29,7 +29,7 @@ Fixed::Fixed(const float num)
 {
 	float fixedPointNum;
 
-	// std::cout << "Float constructor called" << std::endl;
+	std::cout << "Float constructor called" << std::endl;
 	fixedPointNum = roundf(num * (1 << this->FractionalBits));
 	this->fixedPointNum = fixedPointNum;
 }
@@ -37,7 +37,7 @@ Fixed::Fixed(const float num)
 // 복사 생성자
 Fixed::Fixed(const Fixed &f)
 {
-	// std::cout << "Copy constructor called" << std::endl;
+	std::cout << "Copy constructor called" << std::endl;
 
 	*this = f;
 }
@@ -45,7 +45,7 @@ Fixed::Fixed(const Fixed &f)
 // 소멸자
 Fixed::~Fixed()
 {
-	// std::cout << "Destructor called" << std::endl;
+	std::cout << "Destructor called" << std::endl;
 }
 
 // 복사 대입 연산자 오버로딩
@@ -53,7 +53,7 @@ Fixed& Fixed::operator=(const Fixed &f)
 {
 	if (this != &f)
 	{
-		// std::cout << "Copy assignment operator called" << std::endl;
+		std::cout << "Copy assignment operator called" << std::endl;
 		fixedPointNum = f.fixedPointNum;
 	}
 	return (*this);
@@ -62,13 +62,13 @@ Fixed& Fixed::operator=(const Fixed &f)
 // 멤버 함수
 int Fixed::getRawBits(void) const
 {
-	// std::cout << "getRawBits member function called" << std::endl;
+	std::cout << "getRawBits member function called" << std::endl;
 	return (this->fixedPointNum);
 }
 
 void Fixed::setRawBits(int const raw)
 {
-	// std::cout << "setRawBits member function called" << std::endl;
+	std::cout << "setRawBits member function called" << std::endl;
 	this->fixedPointNum = raw;
 }
 
