a2774df75931b72c0dfd82b37681cf4a180c1f42
choiseoji <seoji@MacBook-Air.local>
2024-02-15 00:27:53 +0900

    [feat]: delete 추가

--- 5_cpp_module/cpp05/ex03/main.cpp
+++ 5_cpp_module/cpp05/ex03/main.cpp
@@ -6,32 +6,39 @@
 
 int main(void)
 {
+	AForm *shu_form;
+	AForm *rob_form;
+	AForm *pre_form;
+
 	try
 	{
 		Bureaucrat bureaucrat("seo", 20);
 		Intern someRandomIntern;
-		AForm *form;
 
-		form = someRandomIntern.makeForm("shrubbery creation", "shu");
-		bureaucrat.signForm(*form);
-		bureaucrat.executeForm(*form);
-
-		std::cout << std::endl << "========================" << std::endl << std::endl;
-
-		form = someRandomIntern.makeForm("robotomy request", "rob");
-		bureaucrat.signForm(*form);
-		bureaucrat.executeForm(*form);
+		shu_form = someRandomIntern.makeForm("shrubbery creation", "shu");
+		bureaucrat.signForm(*shu_form);
+		bureaucrat.executeForm(*shu_form);
 
 		std::cout << std::endl << "========================" << std::endl << std::endl;
 
-		form = someRandomIntern.makeForm("presidential pardon", "pre");
-		bureaucrat.signForm(*form);
-		bureaucrat.executeForm(*form);
+		rob_form = someRandomIntern.makeForm("robotomy request", "rob");
+		bureaucrat.signForm(*rob_form);
+		bureaucrat.executeForm(*rob_form);
+
+		std::cout << std::endl << "========================" << std::endl << std::endl;
+
+		pre_form = someRandomIntern.makeForm("presidential pardon", "pre");
+		bureaucrat.signForm(*pre_form);
+		bureaucrat.executeForm(*pre_form);
 
 	}
 	catch(const std::exception& e)
 	{
 		std::cout << e.what() << std::endl;
 	}
+
+	delete shu_form;
+	delete rob_form;
+	delete pre_form;
 	return (0);
 }
