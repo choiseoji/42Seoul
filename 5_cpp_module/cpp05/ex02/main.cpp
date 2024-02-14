253fd721e7ffbe4fbae115b379e25ab2934a217a
choiseoji <seoji@MacBook-Air.local>
2024-02-15 00:23:11 +0900

    [feat]: delete 추가

--- 5_cpp_module/cpp05/ex02/main.cpp
+++ 5_cpp_module/cpp05/ex02/main.cpp
@@ -5,23 +5,27 @@
 
 int main(void)
 {
+	AForm *a_form;
+	AForm *b_form;
+	AForm *c_form;
+
 	try
 	{
 		Bureaucrat bureaucrat("seo", 10);
 
-		AForm *a_form = new ShrubberyCreationForm("shru");   // name이 target, sign: 145, execute: 137
+		a_form = new ShrubberyCreationForm("shru");   // name이 target, sign: 145, execute: 137
 		bureaucrat.signForm(*a_form);   // sign ok
 		bureaucrat.executeForm(*a_form);
 
 		std::cout << std::endl << "========================" << std::endl << std::endl;
 
-		AForm *b_form = new PresidentialPardonForm("pre");   // sign: 25, execute: 5
+		b_form = new PresidentialPardonForm("pre");   // sign: 25, execute: 5
 		bureaucrat.signForm(*b_form);
 		bureaucrat.executeForm(*b_form);
 
 		std::cout << std::endl << "========================" << std::endl << std::endl;
 
-		AForm *c_form = new RobotomyRequestForm("rob");   // sign: 72, execute: 45
+		c_form = new RobotomyRequestForm("rob");   // sign: 72, execute: 45
 		bureaucrat.signForm(*c_form);
 		bureaucrat.executeForm(*c_form);
 
@@ -30,5 +34,9 @@ int main(void)
 	{
 		std::cout << e.what() << std::endl;
 	}
+
+	delete a_form;
+	delete b_form;
+	delete c_form;
 	return (0);
 }
