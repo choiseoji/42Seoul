a6c4e074b767fd1e4413903799dae41b83e08412
choiseoji <choiseoji123@naver.com>
2024-06-22 18:35:51 +0900

    [feat]: BitcoinExchange 기능 구현 끝

--- 5_cpp_module/cpp09/ex00/BitcoinExchange.cpp
+++ 5_cpp_module/cpp09/ex00/BitcoinExchange.cpp
@@ -20,6 +20,16 @@ BitcoinExchange::~BitcoinExchange()
 
 }
 
+bool compare(const std::pair<std::string, std::string>& a, const std::pair<std::string, std::string>& b)
+{
+    return a.first <= b.first;
+}
+
+bool compareL(const std::pair<std::string, std::string>& a, std::string b)
+{
+    return a.first <= b;
+}
+
 // data.csv 파일을 파싱해서 날짜 부분과 데이터 부분을 각각 key, value로 해서 map에 저장해둔다.
 void BitcoinExchange::parsingCSV(void)
 {
@@ -32,31 +42,14 @@ void BitcoinExchange::parsingCSV(void)
         {
             getline(file, data);
 
-            std::string year;
-            std::string month;
-            std::string day;
+            std::istringstream data_stream(data);
+            std::string key;
             std::string value;
 
-            std::istringstream data_stream(data);
-            getline(data_stream, year, '-');
-            getline(data_stream, month, '-');
-            getline(data_stream, day, ',');
+            getline(data_stream, key, ',');
             getline(data_stream, value);
 
-            int fyear = strtod(year.c_str(), NULL);
-            int fmonth = strtod(month.c_str(), NULL);
-            int fday = strtod(day.c_str(), NULL);
-            float fvalue = strtof(value.c_str(), NULL);
-
-            std::vector<float> m;
-
-            m = csv_data[fyear];
-            // vector에 해당 month가 없을 때만 넣어주기
-            if (std::find(m.begin(), m.end(), fmonth) == m.end())
-            {
-                csv_data[fyear].push_back(fmonth);
-            }
-            v[fmonth].push_back({fday, fvalue});
+            csv_data.push_back(std::make_pair(key, value));
         }
     }
     else
@@ -71,14 +64,16 @@ void BitcoinExchange::parsingInFile(std::string file_name)
 
     if (file.is_open())
     {
-        while (file)
+        while (getline(file, data))
         {
-            getline(file, data);
-
             std::string year;
             std::string month;
             std::string day;
             std::string value;
+            std::string date;
+
+            std::istringstream date_stream(data);
+            getline(date_stream, date, ' ');
 
             std::istringstream data_stream(data);
             getline(data_stream, year, '-');
@@ -101,7 +96,7 @@ void BitcoinExchange::parsingInFile(std::string file_name)
             else
             {
                 std::cout << year + "-" + month + "-" + day + "=>" + value + " = ";
-                // std::cout << fvalue * findNearestDate(fyear, fmonth, fday) << std::endl;
+                std::cout << fvalue * findNearestDate(date) << std::endl;
             }
         }
     }
@@ -149,7 +144,18 @@ int BitcoinExchange::checkData(float fyear, float fmonth, float fday, float fval
 }
 
 // csv파일에서 가장 가까운 날짜 찾기
-// float BitcoinExchange::findNearestDate(float fyear, float fmonth, float fday)
-// {
+float BitcoinExchange::findNearestDate(std::string date)
+{
+    int idx;
+    float fvalue;
+    std::vector<std::pair<std::string, std::string> >::iterator it;
+    it = std::lower_bound(csv_data.begin(), csv_data.end(), date, compareL);
+    
+    idx = it - csv_data.begin();
 
-// }
+    if (date != csv_data[idx].first)
+        idx--;
+    fvalue = strtof(csv_data[idx].second.c_str(), NULL);
+
+    return (fvalue);
+}
--- 5_cpp_module/cpp09/ex00/BitcoinExchange.hpp
+++ 5_cpp_module/cpp09/ex00/BitcoinExchange.hpp
@@ -4,7 +4,6 @@
 # include <fstream>
 # include <sstream>
 # include <string>
-# include <map>
 # include <vector>
 # include <algorithm>
 
@@ -18,8 +17,7 @@ enum Error{
 class BitcoinExchange
 {
 private:
-    std::vector<std::pair<int, int>> v[13];   // v[month] = {day, value}
-    std::map<float, std::vector<float>> csv_data;  // year와 매핑되어 있는 month 를 담고있는 벡터
+    std::vector<std::pair<std::string, std::string> > csv_data;
 
 public:
     BitcoinExchange();
@@ -31,6 +29,7 @@ public:
     void parsingInFile(std::string file_name);
     int  checkData(float fyear, float fmonth, float fday, float fvalue);
     // float findNearestDate(float fyear, float fmonth, float fday);
+    float findNearestDate(std::string date);
 };
 
 #endif
