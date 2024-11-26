8e57862bef7e4dbcf0f49b275b776401b5c90008
choiseoji <choiseoji123@naver.com>
2024-11-26 21:22:58 +0900

    feat: wordpress comment time out 해결 + 필요없는 주석 지움

--- 5_inception/srcs/requirements/mariadb/tools/init.sh
+++ 5_inception/srcs/requirements/mariadb/tools/init.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 
-service mariadb start
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+    echo "[INFO] Initializing MariaDB data directory."
+    mkdir -p /run/mysqld
+    chown -R mysql:mysql /run/mysqld
+    mariadb-install-db --user=mysql --datadir=/var/lib/mysql --basedir=/usr --rpm
 
-cat <<EOF > /user.sql
-
+    echo "[INFO] Running MariaDB bootstrap to set up database and user."
+    mysqld --bootstrap --user=mysql <<EOF
 -- 데이터베이스 생성
-CREATE DATABASE IF NOT EXISTS '$WP_DB_NAME';
+CREATE DATABASE IF NOT EXISTS $WP_DB_NAME;
 
 -- 유저 생성 및 암호화 방식 확인
 CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
 
 -- 권한 부여
-GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'%' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON $WP_DB_NAME.* TO '$MYSQL_USER'@'%';
 FLUSH PRIVILEGES;
 EOF
+else
+    echo "[INFO] MariaDB data directory already exists, skipping initialization."
+    chown -R mysql:mysql /var/lib/mysql
+fi
 
-mysql -u root < /user.sql
-rm -rf /user.sql
-
-service mariadb stop
-exec mysqld_safe --user=$MYSQL_USER
+echo "[INFO] Starting MariaDB service."
+exec mysqld_safe --user=mysql
--- 5_inception/srcs/requirements/wordpress/tools/entrypoint.sh
+++ 5_inception/srcs/requirements/wordpress/tools/entrypoint.sh
@@ -1,38 +1,52 @@
 #!/bin/sh
 
 if [ ! -f /usr/local/bin/wp ]; then
-	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-	chmod +x wp-cli.phar
-	mv wp-cli.phar /usr/local/bin/wp
-
-	cd /var/www/html
-
-	rm -rf *
-
-	wp core download --allow-root
-
-	wp config create \
-		--skip-check \
-		--dbname=${WP_DB_NAME} \
-		--dbuser=${MYSQL_USER} \
-		--dbpass=${MYSQL_ROOT_PASSWORD} \
-		--dbhost=${WP_DB_HOST} \
-		--allow-root
-
-	wp core install \
-		--url=${DOMAIN_NAME} \
-		--title=${WP_TITLE} \
-		--admin_user=${WP_ROOT_NAME} \
-		--admin_password=${WP_ROOT_PASSWORD} \
-		--admin_email=${WP_ROOT_EMAIL} \
-		--skip-email \
-		--allow-root
-
-	wp user create ${WP_ADMIN_NAME} ${WP_ADMIN_EMAIL} --user_pass=${WP_ADMIN_PASSWORD} --role=author --allow-root
+    # WP-CLI 설치
+    curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+    chmod +x wp-cli.phar
+    mv wp-cli.phar /usr/local/bin/wp
 fi
 
+cd /var/www/html
+
+# WordPress 다운로드 및 초기화
+if [ ! -f wp-config.php ]; then
+    echo "[INFO] Initializing WordPress setup..."
+    rm -rf *
+
+    wp core download --allow-root
+
+    wp config create \
+        --skip-check \
+        --dbname=${WP_DB_NAME} \
+        --dbuser=${MYSQL_USER} \
+        --dbpass=${MYSQL_ROOT_PASSWORD} \
+        --dbhost=${WP_DB_HOST} \
+        --allow-root
+
+    wp core install \
+        --url=${DOMAIN_NAME} \
+        --title=${WP_TITLE} \
+        --admin_user=${WP_ADMIN_NAME} \
+        --admin_password=${WP_ADMIN_PASSWORD} \
+        --admin_email=${WP_ADMIN_EMAIL} \
+        --skip-email \
+        --allow-root
+else
+    echo "[INFO] WordPress already initialized. Skipping setup."
+fi
+
+# 유저 생성 (존재 여부 확인 후 생성)
+if ! wp user get ${WP_ROOT_NAME} --allow-root > /dev/null 2>&1; then
+    echo "[INFO] Creating new user '${WP_ROOT_NAME}'..."
+    wp user create ${WP_ROOT_NAME} ${WP_ROOT_EMAIL} --user_pass=${WP_ROOT_PASSWORD} --role=author --allow-root
+else
+    echo "[INFO] User '${WP_ROOT_NAME}' already exists. Skipping user creation."
+fi
+
+# 퍼미션 설정
 chmod -R 777 /var/www
 
+# PHP-FPM 실행
 mkdir -p /run/php
-
 php-fpm7.4 -F
