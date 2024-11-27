b821518d781afbf0a4ebecd6f0519386ed8c2a45
choiseoji <choiseoji123@naver.com>
2024-11-27 18:24:16 +0900

    feat: mariadb init 파일 수정

--- 5_inception/srcs/requirements/mariadb/tools/init.sh
+++ 5_inception/srcs/requirements/mariadb/tools/init.sh
@@ -1,27 +1,16 @@
 #!/bin/bash
 
-if [ ! -d "/var/lib/mysql/mysql" ]; then
-    echo "[INFO] Initializing MariaDB data directory."
-    mkdir -p /run/mysqld
-    chown -R mysql:mysql /run/mysqld
-    mariadb-install-db --user=mysql --datadir=/var/lib/mysql --basedir=/usr --rpm
+service mariadb start
 
-    echo "[INFO] Running MariaDB bootstrap to set up database and user."
-    mysqld --bootstrap --user=mysql <<EOF
--- 데이터베이스 생성
-CREATE DATABASE IF NOT EXISTS $WP_DB_NAME;
-
--- 유저 생성 및 암호화 방식 확인
+cat <<EOF > /user.sql
+CREATE DATABASE IF NOT EXISTS wordpress;
 CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
-
--- 권한 부여
-GRANT ALL PRIVILEGES ON $WP_DB_NAME.* TO '$MYSQL_USER'@'%';
+GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
 EOF
-else
-    echo "[INFO] MariaDB data directory already exists, skipping initialization."
-    chown -R mysql:mysql /var/lib/mysql
-fi
 
-echo "[INFO] Starting MariaDB service."
-exec mysqld_safe --user=mysql
+mysql -u root < /user.sql
+rm -rf /user.sql
+
+service mariadb stop
+exec mysqld_safe --user=$MYSQL_USER
